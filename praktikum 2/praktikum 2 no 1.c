481d1c55bed042a295e597fc1c3b03c1fe81701f
argarafiar <89307621+argarafiar@users.noreply.github.com>
2021-10-01 18:21:43 +0700

    menambahkan program data ulangan (hacktoberfest)

--- praktikum 2/praktikum 2 no 1.c
+++ praktikum 2/praktikum 2 no 1.c
@@ -6,9 +6,30 @@ int main()
     printf("==========================================\n");
     printf("        --PROGRAM CETAK NAMA--\n");
     printf("==========================================\n");
-    printf("NAMA  : \n");
-    printf("KELAS : IT A\n");
-    printf("NRP   : \n");
-    ;
+//    printf("NAMA  : \n");
+//    printf("KELAS : IT A\n");
+//    printf("NRP   : \n");
+    
+    printf("=====soal pendahuluan no-4=====\n");
+    int nilai, i, ulangan, minimal=100, maksimal=0;
+    float rata=0;
+
+    printf("Ada berapa ulangan : ");
+    scanf("%d", &ulangan);
+
+    for(i=1; i<=ulangan; i++)
+    {
+        printf("Masukkan nilai ulangan ke-%d : ", i);
+        scanf("%d", &nilai);
+        if(nilai > maksimal)
+            maksimal=nilai;
+        else if(minimal > nilai)
+            minimal=nilai;
+        rata+=nilai;
+    }
+    printf("\nNilai maksimal : %d\n", maksimal);
+    printf("Nilai minimal : %d\n", minimal);
+    printf("Nilai rata-rata : %.2f\n", rata/ulangan);
+
     return 0;
 }
